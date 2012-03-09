27eec6d5c4d6db6ec983d4d9862348180c6cfbae
Thiago Baptista <contato@thiagobaptista.com>
2012-03-09 15:43:49 -0300

    O esqueleto ambulante

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.2'
+gem 'pg'
+gem 'jquery-rails'
+gem 'nifty-generators'
+
+group :assets do
+    gem 'sass-rails',   '~> 3.2.3'
+    gem 'coffee-rails', '~> 3.2.1'
+    gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+    gem 'capybara'
+    gem 'database_cleaner'
+    gem 'cucumber-rails'
+    gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Estudo1RorOp1::Application.config.secret_token = 'efe1993a82a4d505bc19466aaf83c81294a7ed66630b3552c93c75049eb5c159b2bb31921696809a40007155f24798efdedf8f5cf563c94028b36d18cbb40edd'
